b49def4d34357f5dab317766fb7c11a905bd9774
hs10530 <xalinteyan@gmail.com>
2023-07-11 16:09:51 -0400

    feat: update some issues

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -32,8 +32,7 @@ function Projects() {
               isBlog={false}
               title="Chatify"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              ghLink="https://github.com/czy30121/react-chat-app"
             />
           </Col>
 
@@ -43,8 +42,7 @@ function Projects() {
               isBlog={false}
               title="Bits-0f-C0de"
               description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              ghLink="https://github.com/czy30121/react-blog-app"
             />
           </Col>
 
@@ -54,8 +52,7 @@ function Projects() {
               isBlog={false}
               title="Editor.io"
               description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              ghLink="https://github.com/czy30121/react-code-editor"           
             />
           </Col>
 
@@ -65,7 +62,7 @@ function Projects() {
               isBlog={false}
               title="Alma"
               description="An interactive playground for generative graphics. Combine nodes & logic into WebGL shaders - all through an intuitive no-code interface."
-              ghLink="https://github.com/hs10530/"
+              ghLink="https://github.com/czy30121/react-alma-app"
             />
           </Col>
 
@@ -75,7 +72,7 @@ function Projects() {
               isBlog={false}
               title="API Platform Admin"
               description="API Platform Admin is a tool to automatically create a beautiful (Material Design) and fully-featured administration interface for any API supporting the Hydra Core Vocabulary or exposing an OpenAPI documentation, including but not limited to all APIs created using the API Platform framework."
-              ghLink="https://github.com/hs10530/"
+              ghLink="https://github.com/czy30121/react-admin-api"
             />
           </Col>
 
@@ -85,7 +82,7 @@ function Projects() {
               isBlog={false}
               title="Next.js AI Chatbot"
               description=" An open-source AI chatbot app template built with Next.js, the Vercel AI SDK, OpenAI, and Vercel KV."
-              ghLink="https://github.com/hs10530/"
+              ghLink="https://github.com/czy30121/react-ai-chatbot"
             />
           </Col>
           
@@ -95,7 +92,7 @@ function Projects() {
               isBlog={false}
               title="VueWordPress Theme"
               description="A true WordPress theme with the guts ripped out and replaced with Vue. Looking to create a Vue-powered WordPress plugin? I've got a starter for that too!"
-              ghLink="https://github.com/hs10530/"
+              ghLink="https://github.com/czy30121/vuejs-wordpress-theme-starter"
             />
           </Col>
           
@@ -105,7 +102,7 @@ function Projects() {
               isBlog={false}
               title="WordPress Author Theme"
               description="This theme was originally developed for a closed group of authors and not intended for a public release. This is still reflected in the code, which takes several liberties not considered best practice."
-              ghLink="https://github.com/hs10530/"
+              ghLink="https://github.com/czy30121/wordpress-author-theme"
             />
           </Col>
         </Row>
